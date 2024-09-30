b39e516acaab29123d6337570edf76a6d39e661c
Kieran <kieran.hayter2021@gmail.com>
2024-09-30 20:10:02 +0100

    Create main and run locally

--- OOP1app.java
+++ OOP1app.java
@@ -1,5 +1,6 @@
 public class OOP1app {
     public static void main(String[] args) {
-      System.out.println("Welcome to the Invoice Total Calculator");
+
+        System.out.println("Welcome to my OOP1 project application");
   }
 }
